99f1b1d452bbdf7452b508b2c011d6e23491eb7f
tawhidulkhan <tawhidul.islam@branstation-23.com>
2025-09-16 14:53:22 +0600

    change: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/form-builder/'  // replace REPO_NAME with your GitHub repository name
+  base: './'  // replace REPO_NAME with your GitHub repository name
 })
